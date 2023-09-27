075cfc83902c27ead268ee4cf31432eaa39bfcb7
aji_hermansya <ajihermansya14@gmail.com>
2023-09-27 11:36:11 +0700

    add Kapt-kotlin

--- app/src/main/java/com/rumahsosial/gituse/detail/local/UserDao.kt
+++ app/src/main/java/com/rumahsosial/gituse/detail/local/UserDao.kt
@@ -1,12 +1,13 @@
 package com.rumahsosial.gituse.detail.local
 
 import androidx.lifecycle.LiveData
+import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.rumahsosial.gituse.model.ResponseUserGithubs
-
+@Dao
 interface UserDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insert(user: ResponseUserGithubs.Item)
